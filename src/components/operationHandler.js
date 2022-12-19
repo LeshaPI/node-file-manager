55545d07caaff714090454f9be3162404dfe96b3
LeshaPI <pialeksej5@gmail.com>
2022-12-19 18:25:28 +0300

    fix: names

--- src/components/operationHandler.js
+++ src/components/operationHandler.js
@@ -2,9 +2,10 @@ import { ls } from "../operations/list.js";
 import { cd, up } from "../operations/navigation.js"
 import { showCurrentPath } from "./currentPath.js";
 import { separateArg } from "./separateArg.js";
-import { read, create, renameFile, copy, move, delite } from "../operations/fsOperation.js";
+import { read, create, renameFile, copy, move, delite } from "../operations/fsOperations.js";
 import { normalizePath } from "./normalizePath.js";
-import { osOperation } from "../operations/osOperation.js";
+import { osOperation } from "../operations/osOperations.js";
+import { OPERATION_FAILED } from "../utils/consts.js";
 
 export const operationHandler = ( operation ) => {
 
@@ -13,34 +14,36 @@ export const operationHandler = ( operation ) => {
 
 
     if(!err){
-      try {
-        
-        switch(op){
-            case 'up': up();
-            break;
-            case 'cd': cd(args);
-            break;
-            case 'ls': ls();
-            break;
-            case 'cat': read(args); 
-            break;
-            case 'add': create(args);
-            break;
-            case 'rn': renameFile(args);
-            break;
-            case 'cp': copy(args);
-            break;
-            case 'mv': move(args);
-            break;
-            case 'rm': delite(args);
-            break;
-            case 'os': osOperation(args);
-            break
-        }
-
-    } catch (error) {
-        
-    }   
+        try {
+          
+            switch(op){
+                case 'up': up();
+                break;
+                case 'cd': cd(args);
+                break;
+                case 'ls': ls();
+                break;
+                case 'cat': read(args); 
+                break;
+                case 'add': create(args);
+                break;
+                case 'rn': renameFile(args);
+                break;
+                case 'cp': copy(args);
+                break;
+                case 'mv': move(args);
+                break;
+                case 'rm': delite(args);
+                break;
+                case 'os': osOperation(args);
+                break
+                case 'compress':
+                break;
+            }
+  
+        } catch (error) {
+           console.log(OPERATION_FAILED);  
+        }   
     }
     
     showCurrentPath();
